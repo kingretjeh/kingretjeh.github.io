d6a4b687cc8ffdf1af390f106ac90dd10e6a4e61
kingretjeh <126808458+kingretjeh@users.noreply.github.com>
2023-03-03 01:19:06 +0700

    Update config.js

--- assets/config.js
+++ assets/config.js
@@ -4,6 +4,16 @@ let config = {
   "Description": "xixixixixixixi",
   "links": [
     {
+      "Title": "TOPUP MURAH",
+      "URL": "http://sitopup.my.id",
+      "icon_classes": "fa fa-globe",
+    },
+    {
+      "Title": "Take Free",
+      "URL": "https://kingretjeh.github.io/404.html",
+      "icon_classes": "fa fa-link"
+    },
+    {
       "Title": "Support",
       "URL": "https://sociabuzz.com/kingretjeh/support",
       "icon_classes": "fa fa-gift"
