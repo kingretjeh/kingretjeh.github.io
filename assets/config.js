33c8a08e16c99b8bd1230a1de641d0afc2127731
kingretjeh <126808458+kingretjeh@users.noreply.github.com>
2023-03-03 00:27:20 +0700

    Add files via upload

---
+++ assets/config.js
@@ -0,0 +1,22 @@
+let config = {
+  "Title": "SI TOPUP - INFO",
+  "Name": "SI TOPUP",
+  "Description": "Website Topup Game dan Entertaiment #1 Di Indonesia",
+  "links": [
+    {
+      "Title": "TOPUP 24 JAM",
+      "URL": "http://sitopup.my.id",
+      "icon_classes": "fa fa-globe",
+    },
+    {
+      "Title": "Join Reseller",
+      "URL": "https://sitopup.my.id/auth/register",
+      "icon_classes": "fa fa-user-plus",
+    },
+    {
+      "Title": "Support",
+      "URL": "https://sociabuzz.com/sitopup/support",
+      "icon_classes": "fa fa-gift"
+    },
+  ]
+}
