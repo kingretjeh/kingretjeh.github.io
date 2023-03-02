d90f041e94d0d304d21dd61d31312b579bfaf8fe
kingretjeh <126808458+kingretjeh@users.noreply.github.com>
2023-03-03 00:50:12 +0700

    Update config.js

--- assets/config.js
+++ assets/config.js
@@ -1,21 +1,11 @@
 let config = {
-  "Title": "SI TOPUP - INFO",
-  "Name": "SI TOPUP",
-  "Description": "Website Topup Game dan Entertaiment #1 Di Indonesia",
+  "Title": "KING RETJEH",
+  "Name": "KING RETJEH",
+  "Description": "xixixixixixixi",
   "links": [
     {
-      "Title": "TOPUP 24 JAM",
-      "URL": "http://sitopup.my.id",
-      "icon_classes": "fa fa-globe",
-    },
-    {
-      "Title": "Join Reseller",
-      "URL": "https://sitopup.my.id/auth/register",
-      "icon_classes": "fa fa-user-plus",
-    },
-    {
       "Title": "Support",
-      "URL": "https://sociabuzz.com/sitopup/support",
+      "URL": "https://sociabuzz.com/kingretjeh/support",
       "icon_classes": "fa fa-gift"
     },
   ]
