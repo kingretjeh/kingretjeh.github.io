466d7ae3ec2671a9ded319fd979c4cff6af4db40
kingretjeh <126808458+kingretjeh@users.noreply.github.com>
2023-10-20 23:30:09 +0700

    Update config.js

--- assets/config.js
+++ assets/config.js
@@ -9,8 +9,8 @@ let config = {
       "icon_classes": "fa fa-globe",
     },
     {
-      "Title": "Canva Juni",
-      "URL": "https://www.canva.com/brand/join?token=g95Zwm1JQBGZZr3KOLzGyw&referrer=team-invite",
+      "Title": "Canva Oktober",
+      "URL": "https://www.canva.com/brand/join?token=HIh3Vg-wDQcJBmeSzlAzew&referrer=team-invite",
       "icon_classes": "fa fa-link"
     },
     {
